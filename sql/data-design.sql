00870eeb9c4ee96b8a5524ff1f2a6368bd45b9db
Carl Hernandez <carl.hernandez89@gmail.com>
2020-05-04 16:46:18 -0600

    added sql commands

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -4,3 +4,29 @@ DROP TABLE IF EXISTS {}
 DROP TABLE IF EXISTS {}
 DROP TABLE IF EXISTS {}
 
+CREATE TABLE card(
+    cardId BINARY(16) not null,
+    cardName VARCHAR(150) not null,
+    cardPictureUrl VARCHAR(150),
+    cardPrice DECIMAL(6,2) not null,
+    UNIQUE(cardName),
+    PRIMARY KEY(cardId)
+);
+
+CREATE TABLE profile(
+    profileId BINARY(16) not null,
+    profileEmail VARCHAR(128) not null,
+    profileHash CHAR(32),
+    UNIQUE(profileEmail),
+    PRIMARY KEY (profileId)
+);
+
+CREATE TABLE watchList(
+    watchListCardId BINARY(16) not null,
+    watchListProfileId BINARY(16) not null,
+    INDEX (watchListCardId),
+    INDEX (watchListProfileId),
+    FOREIGN KEY(watchListCardId) REFERENCES card(cardId),
+    FOREIGN KEY(watchListProfileId) REFERENCES profile(profileId),
+    PRIMARY KEY(watchListCardId, watchListProfileId)
+);
