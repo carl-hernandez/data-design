c5ebcd86b8d25a7744977fd8b9db5cb9289f18b5
Carl Hernandez <carl.hernandez89@gmail.com>
2020-05-17 21:33:50 -0600

    fixed commands

--- sql/oml.sql
+++ sql/oml.sql
@@ -9,7 +9,7 @@
 --insert cards into card table
 INSERT INTO card (cardId, cardName, cardPictureUrl, cardPrice) VALUES (UNHEX("4441294d90b1471d86bbea675f8a72fa"), "Forbidden Alchemy", "https://static.mtgstocks.com/cardimages/226758.jpg", 0.12);
 INSERT INTO card (cardId, cardName, cardPictureUrl, cardPrice) VALUES (unhex("8929aaeece8041e0a7d6662bf45cd961"), "Black Lotus", "https://static.mtgstocks.com/cardimages/298.jpg", 4200.00);
-INSERT INTO card (cardID, cardName, cardPictureUrl, cardPrice) VALUES (UNEHX("5cebc358b9f340299cd84a25b9b11b52"), "Blacker Lotus", "https://static.mtgstocks.com/cardimages/9764.jpg", 6.41);
+INSERT INTO card (cardID, cardName, cardPictureUrl, cardPrice) VALUES (UNHEX("5cebc358b9f340299cd84a25b9b11b52"), "Blacker Lotus", "https://static.mtgstocks.com/cardimages/9764.jpg", 6.41);
 
 --insert profile into profile table
 INSERT INTO profile (profileId, profileEmail, profileHash) VALUES (UNHEX("22c43a7b981443c4bd98002262e498d1"), "timmy@bankrupt.com", "$argon2i$v=19$m=16,t=2,p=1$bHRNSVRTdGYzaWVIWEt1ZA$1nWTFGy6cIjhH9+Ah4IRmA");
@@ -26,10 +26,10 @@ DELETE FROM card WHERE cardId = unhex ("8929aaeece8041e0a7d6662bf45cd961");
 INSERT INTO watchList VALUES ( UNHEX("4441294d90b1471d86bbea675f8a72fa"), UNHEX("22c43a7b981443c4bd98002262e498d1"));
 
 --select card from card table
-SELECT cardId, cardName, cardPrice from card WHERE cardName cardId= unhex("5cebc358b9f340299cd84a25b9b11b52");
+SELECT cardId, cardName, cardPrice FROM card WHERE cardId= unhex("5cebc358b9f340299cd84a25b9b11b52");
 
 --innerjoin email onto watchlist
-SELECT profile.profileEmail, watchList.watchlistProfileId FROM watchlist INNER JOIN profile on watchlist.watchlistProfileId = profile.profileId WHERE watchlistProfileId = unhex("22c43a7b981443c4bd98002262e498d1");
+SELECT profile.profileEmail, watchList.watchListProfileId FROM watchList INNER JOIN profile on watchList.watchListProfileId = profile.profileId WHERE watchlistProfileId = unhex("22c43a7b981443c4bd98002262e498d1");
 
 --count number of likes on DDC Tweet
 --SELECT tweet.tweetId, like.likeTweetId FROM like INNERJOIN tweet on tweet.tweetId = like.likeTweetId WHERE unhex = ("37480a7e31ac43b7a6ea364dc46a615b");
